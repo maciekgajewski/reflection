35c1c4a82bc8f7aa8bcf44285eef33d6633720a4
Maciek Gajewski <maciej.gajewski0@gmail.com>
2014-06-26 21:10:02 +0200

    some more work on parser

--- generator/generator.py
+++ generator/generator.py
@@ -17,37 +17,38 @@ class Scope(object):
 		self.currentFile = None
 
 	def add(self, line, indent):
-		if self.subscope:
-			if indent =< self.indent:
-				self.subscope = None
-			else:
-				self.subscope.add(line):
+		#print("Scope: add, indent=%d, my indent=%d, line=%s" % (indent, self.indent, line))
+		if indent <= self.indent:
+			self.subscope = None
+		if indent == (self.indent+1):
+			self.on_line(line)
+		else:
+			if self.subscope:
+				self.subscope.add(line, indent)
 
+	def on_line(self, line):
+		#print("Scope, line: %s" % line)
 		# try record
-		if indent == self.indent:
-			match = re.match(re_record, line)
-			if match:
-				return self.on_record(match, line)
+		match = re.match(re_record, line)
+		if match:
+			return self.on_record(match, line)
 
-		return False
 
 	def on_record(self, match, line):
-			indent = len(match.group(1))/2
-			file = match.group(2)
-			cos = match.group(3)
-			name = match.group(4)
+		file = match.group(1)
+		cos = match.group(2)
+		name = match.group(3)
 
-			if file != 'line':
-				self.currentFile = os.path.abspath(file)
-				#print('current file set to %s' % self.currentFile)
+		if file != 'line':
+			self.currentFile = os.path.abspath(file)
+			#print('current file set to %s' % self.currentFile)
 
-			print('record: %s, indent: %d, my indent=%d, my name=%s' % (name, indent, self.indent, self.name))
+		print('record: %s, indent: %d, my indent=%d, my name=%s' % (name, indent, self.indent, self.name))
 
-			if indent == self.indent + 1 and (self.currentFile == None or self.currentFile.startswith(self.sourceDir)):
-				print('Record: "%s"' % name)
-				return True
+		if self.currentFile == None or self.currentFile.startswith(self.sourceDir):
+			print('Record: "%s"' % name)
 
-			return False
+		return False
 
 class Record(Scope):
 	def __init__(self, sourceDir, name, indent):
@@ -60,32 +61,27 @@ class Namespace(Scope):
 		print('Created namespace "%s": indent: %d' % (name, indent))
 		self.namespaces = {}
 
-	def add(self, line):
-		# pass to sub-scope, if set
-		if Scope.add(self, line) == False:
-			# try namespace
-			match = re.match(re_namespace, line)
-			if match:
-				self.on_namespace(match, line)
-
+	def on_line(self, line):
+		#print("Namespace, line=%s" % line)
+		# try namespace
+		match = re.match(re_namespace, line)
+		if match:
+			self.on_namespace(match, line)
 
 	def on_namespace(self, match, line):
-			indent = len(match.group(1))/2
-			file = match.group(2)
-			name = match.group(3)
+		file = match.group(1)
+		name = match.group(2)
 
-			#print('namespace %s: indent: %d, file: %s' % (name, indent, file))
+		print('namespace %s: file: %s' % (name, file))
 
-			if file != 'line':
-				self.currentFile = os.path.abspath(file)
-				#print('current file set to %s' % self.currentFile)
+		if file != 'line':
+			self.currentFile = os.path.abspath(file)
 
+		if self.currentFile == None or self.currentFile.startswith(self.sourceDir):
+			if name not in self.namespaces:
+				self.namespaces[name] = Namespace(self.sourceDir, name, self.indent+1)
 
-			if indent == self.indent + 1 and (self.currentFile == None or self.currentFile.startswith(self.sourceDir)):
-				if name not in self.namespaces:
-					self.namespaces[name] = Namespace(self.sourceDir, name, indent)
-
-				self.subscope = self.namespaces[name]
+			self.subscope = self.namespaces[name]
 
 
 
@@ -100,7 +96,9 @@ class Generator(object):
 		m = re.match(re_indent, line)
 		if m:
 			indent = m.group(1)
-		self.root.add(line[len(indent):], indent/2)
+			self.root.add(line[len(indent)-1:], int(len(indent)/2))
+		else:
+			print("NO INDENT: %s" % line)
 
 	def dump(self, out):
 		pass
