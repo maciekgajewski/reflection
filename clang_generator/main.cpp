25a41f1f2b1ca4cafabc462c8a358e3b97ba5613
Maciek Gajewski <maciej.gajewski0@gmail.com>
2014-07-27 19:26:45 +0200

    Compiled forst clang-using binary

--- clang_generator/main.cpp
+++ clang_generator/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main(int, char**)
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/CompilerInvocation.h>
+#include <clang/Basic/Version.h>
+
+int main(int argc, char** argv)
 {
-	std::cout << "Hello!" << std::endl;
+	clang::CompilerInstance ci;
+
+	clang::CompilerInvocation in;
+	//clang::CompilerInvocation::CreateFromArgs(in, argv+1, argv + argc, ci.getDiagnostics());
+
+
+
+	//ci.setInvocation(&in);
+	//ci.setTarget(clang::TargetInfo::CreateTargetInfo( ci.getDiagnostics(), in.getTargetOpts()));
+
 }
